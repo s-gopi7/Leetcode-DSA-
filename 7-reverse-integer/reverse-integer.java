ad95b1abd5c21aa7841db239621644ccffed10bc
s-gopi7 <sanaganigopi77@gmail.com>
2026-03-25 19:54:03 +0530

    Time: 1 ms (99.93%) | Memory: 42.5 MB (87.64%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        int r=0;
+        while(x!=0){
+            int d=x%10;
+            x=x/10;
+            if(r>Integer.MAX_VALUE/10||r<Integer.MIN_VALUE/10){
+                return 0;
+            }
+            r=r*10+d;
+        
+        }
+        return r;
+            
+        
+    }
+}
